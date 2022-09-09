ca7ebbeb633bedf90d094b483cf4c049827f41c1
jgbpessoa <jgbpessoa@gmail.com>
2022-09-09 12:24:28 -0500

    Navigating Programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("Your order was placed");
+    router.push("/product");
+  };
   return (
     <>
       <h1>Home Page</h1>
@@ -11,6 +18,8 @@ function Home() {
       <Link href="/product">
         <a>Products</a>
       </Link>
+      <br />
+      <button onClick={handleClick}>Place Order</button>
     </>
   );
 }
